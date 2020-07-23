3a1718ce7fedfc012bd0922ce62353e551b29669
kirrabelloche <kirraridibo@gmail.com>
2020-07-23 02:27:38 +0200

    update

--- resources/views/cours/single.blade.php
+++ resources/views/cours/single.blade.php
@@ -8,12 +8,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
+            <h1 class="m-0 text-dark">Forum cours</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
 
-              <li class="breadcrumb-item active"> Tableau de bord</li>
+              <li class="breadcrumb-item active">Forum cours</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
